19161f0852a3cb06465dc532166150714b9cc6dc
Hussainzz <shkhussain12@gmail.com>
2022-03-27 18:58:29 +0530

    removed dev

--- lotteryapp/src/redux/store.js
+++ lotteryapp/src/redux/store.js
@@ -1,14 +1,14 @@
 import {createStore,applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
+//import {composeWithDevTools} from 'redux-devtools-extension';
 import allReducers from './reducers';
 
 
 const middleware = [thunk]
 const store = createStore(
     allReducers,
-    (process.env.REACT_APP_ENV !== "DEVELOPMENT")?compose(applyMiddleware(...middleware)):
-    composeWithDevTools(applyMiddleware(...middleware))
+   // (process.env.REACT_APP_ENV !== "DEVELOPMENT")?compose(applyMiddleware(...middleware)):
+    compose(applyMiddleware(...middleware))
 );
 
 export default store;
